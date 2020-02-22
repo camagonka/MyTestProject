150c2f21fd4f03f4b25da5cc2842acf393caaab3
Marina_Panteleeva <marinawork1989@gmail.com>
2020-02-22 13:32:57 +0300

    Initial commit

---
+++ src/main/java/MyFirstProgram.java
@@ -0,0 +1,19 @@
+package main.java;
+
+public class MyFirstProgram {
+
+    public static void main(String[] args) {
+
+        hello("world");
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+    }
+
+    private static void hello(String somebody) {
+        System.out.println("Hello 123 " + somebody);
+    }
+
+    private static double area(Square s){
+        return s.l+s.l;
+    }
+}
---
+++ src/main/java/Square.java
@@ -0,0 +1,9 @@
+package main.java;
+
+public class Square {
+    public double l;
+
+    public Square(double l) {
+        this.l = l;
+    }
+}
